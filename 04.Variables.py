26b0f50859610165ff99e70b92d6597c56e9887e
suchitanigam <suchitanigam2302@gmail.com>
2026-01-18 13:40:23 +0530

    04.Variables

---
+++ 04.Variables.py
@@ -0,0 +1,27 @@
+#Python has no command for declaring a variable.
+#A variable is created the moment you first assign a value to it.
+
+X = 1          #Integer Variable
+X= "Suchi"   #String Variable
+print(X)
+
+name = "Suchita"
+language = "Python"
+
+print("My name is",name)
+print(name,"Suchita is learning",language)
+print(language,"is aewsome!")
+
+#Casting        specify the data type of a variable
+x= int(2)
+y= str(3)
+z= float(4)
+
+print(x,y,z)
+
+#the Type function
+x = 5
+y = "Data Engoneering"
+
+print(type(x))
+print(type(y))
