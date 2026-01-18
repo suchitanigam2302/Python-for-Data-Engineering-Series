a11589b7cd0ac3638c6030ab4b8ca1ca5a576ce0
suchitanigam <suchitanigam2302@gmail.com>
2026-01-18 14:18:09 +0530

    05.InputFunctions

---
+++ 05.InputFunction.py
@@ -0,0 +1,11 @@
+name = input("Enter your name:")
+country = "India'"
+print(name,"is from",country)
+
+
+# LOgin page
+username= input("Enter your name : ")
+paasword= input("Enter your password: ")
+print("Username is:",username)
+print("Password is:",paasword)
+
