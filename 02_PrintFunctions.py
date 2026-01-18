6e220169b990a95ebc694601135119c53fcf73ba
suchitanigam <suchitanigam2302@gmail.com>
2026-01-18 12:21:06 +0530

    03_EscapeCharacters

---
+++ 02_PrintFunctions.py
@@ -0,0 +1,13 @@
+print("Hi Python")
+
+print('Hello I am learnind Python')
+
+
+print("---------------------------------")
+print(  "DATA ENGINEERING PYTHON SERIES"  )
+print("---------------------------------")
+
+#THE BELOW GIVEN LOC WILL PRINT THE STATEMENT WITHOUT NEW LINE         end = " " IS USED TO AVOID NEW LINE
+print ("Hello I am Suchita", end=" ")
+print("I am on the path to become Data Engineer")
+
---
+++ 03_EscapeCharacters.py
@@ -0,0 +1,44 @@
+#  TO INSERT CHARACTERS THAT ARE ILLEGAL IN A STRING LITERAL, WE USE ESCAPE CHARACTERS
+#IT IS FOLLOERD BY A BACKSLASH '\'AND A CHARACTER WHICH IS TO BE INSERTED
+# SOME COMMON ESCAPE CHARACTERS ARE:
+# \'  SINGLE QUOTE     
+# \"  DOUBLE QUOTE
+# \\  BACKSLASH 
+# \b  BACKSPACE 
+# \f  FORM FEED 
+# \n  NEW LINE (ADDS A NEW LINE)
+# \t  TAB  (ADDS A TAB SPACE)
+
+#SINGLE QUOTE \'
+print('I\'m  Suchita.' )
+
+#DOUBLE QUOTE \"
+print("He said \"Python is awesome\" ")
+print('He saird"Python is awesome" ')
+
+#BACKSLASH \\
+print("This will insert one backslash \\ in the string.")
+print("Path : C:\\Users\\Suchita\\Documents")
+
+#NEW LINE \n
+print("Hello World!\n Welcome to Python Programming.")
+print("Hello World!\n Welcome to Python Programming.")
+
+#TAB \t
+print("Name\tAge\tCountry")
+
+print("Alice\t30\tUSA")
+
+#CAARRIAGE RETURN \r
+print("Hello World! Welcome to Python Programming.\rHi")
+
+#BACKSPACE \B
+print("Hello \bWorld!")
+
+
+#PRINT QUESTION CHALLENGE
+
+print("""Your Learning Path :
+      \t-Python Basics
+      \t-Data Engineering
+      \t-AI""")
