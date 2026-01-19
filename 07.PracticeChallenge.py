702d93248c40b0e406aa6af6fbb91d9a6d94889b
suchitanigam <suchitanigam2302@gmail.com>
2026-01-19 06:46:56 +0530

    07.PracticeChallenge

---
+++ 07.PracticeChallenge.py
@@ -0,0 +1,34 @@
+#Practice Challenge
+#I'm here solving a practice challenge related to all we done till now in Python for Data Engineering Series
+#The challenge is to design Student Profile Generator 
+# We have to create variables to store student information like Name, Age, Height, Percentage, City,IS_Student_Active
+#Then we have to print all the information stored in the variables along with their data type
+
+print("------------------------------------------------")
+print("    STUDENT PROFILE GENERATOR    ")
+print("------------------------------------------------") 
+
+name = input("Enter Student Name\t:")
+age =int(input("Enter Student Age\t:"))
+Percentage = float(input("Enter Student Percentage:"))
+City=input("Enter Student City\t:")
+Is_Student_Active=input("Active (True/False):")
+Is_Student_Actice=input()=="True"
+
+
+print("------------------------------------------------")
+print("\tStudent Profile")
+print("------------------------------------------------")
+
+print("Name\t:",name)
+print("Age\t:",age)
+print("Percant:",Percentage)
+print("City\t:",City)
+print("Active\t:",Is_Student_Active)
+
+print("\nThank You For Regisetring Your Profile")
+
+
+
+
+
