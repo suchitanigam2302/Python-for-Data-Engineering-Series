a9287f33c12922738c6ea186c0cc5d2a6d4c5336
suchitanigam <suchitanigam2302@gmail.com>
2026-01-19 22:30:29 +0530

    08.StringFunctions

---
+++ 08.StringFunctions.py
@@ -0,0 +1,45 @@
+#Types Functions in Python
+
+name = "Suchita"
+print(type(name))
+
+age = 19
+print(type(age))
+print("Your Age is:" + str(age))
+
+#Math Functions
+#1.length function
+password = input("Enter your password:")
+print(len(password))
+
+if(len(password)<8):
+    print("Password is too short")
+else:
+    print("Password is correct")
+
+
+#2.Count function
+message = '''I'm Learning Python
+Python is very powerful language
+Python is used in many applications'''
+
+print(message.count("is"))
+
+#Transformations Functions
+#1.Replace function
+price = "$1,233.99"
+print(price.replace("$","").replace(",",""))
+
+price1 = "+49 (176) 123-4567"
+print(price1.replace("(","").replace(")","").replace("-","").replace(" ","").replace("+","00"))
+
+#2.Concatenation function
+First_name = "Suchita"
+Last_name = "Nigam"
+Last_name1=First_name + " " + Last_name
+print(Last_name1)
+
+folder = "C:/Users/Suchita/"
+file = "data.csv"
+full_file_path = folder + file
+print(full_file_path)
