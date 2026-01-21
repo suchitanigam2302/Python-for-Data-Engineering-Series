fd5236ef3664347b7a633398c69b14ae8adbed72
suchitanigam <suchitanigam2302@gmail.com>
2026-01-22 00:44:39 +0530

    Type Functions

---
+++ 09.NumericFunctions.py
@@ -0,0 +1,30 @@
+#Type Functions
+x =10
+y = 3
+z =2 + 3j
+
+print("Value of x:",x)
+print("Type of x:",type(x))
+print("Value of y:",y)
+print("Type of y:",type(y))
+print("Value of z:",z)
+print("Type of z:",type(z))
+
+#Integer Conversion
+a = "24"
+print(type(a))
+print(a * 3)
+b = int(a)
+print(type(b))
+print( b * 3)
+
+
+#Float Conversion
+c = "3.14"
+print(float(c))
+
+#Complex Number
+
+d = 3 #real part
+e = 4 #imaginary part
+print(complex(d,e))
