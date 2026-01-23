e5870be8a9667d2259c56b5c0938c371fa8279de
suchitanigam <suchitanigam2302@gmail.com>
2026-01-23 11:56:26 +0530

    BooleanFunctions

--- 11.BooleanFunctions.py
+++ 11.BooleanFunctions.py
@@ -10,18 +10,3 @@ print(bool(""))
 print(bool(None))
 
 
-email = "suchi@gmail.com"
-phone = "09808098"
-username = "suchi23"
-
-#allows registration if any field is filled
-print(any([email,phone,username]))
-
-#allows registration if all the fiels are filled
-print(all([email,phone,username]))
-
-print(isinstance(123,int))
-print(isinstance(True,str))
-
-print("Hello".endswith("o"))
-print("Hello".startswith("o"))
