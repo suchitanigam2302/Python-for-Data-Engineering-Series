f66d0fb906af6e77eec1293d35889be23b1764ff
suchitanigam <suchitanigam2302@gmail.com>
2026-01-24 06:46:25 +0530

    PracticeQuestions

--- 12.LogicalOperators.py
+++ 12.LogicalOperators.py
@@ -8,3 +8,13 @@ print(3 > 1 and 5 > 1)
 print(3 > 1 or 5 < 1)
 print(3 < 1 or 5 < 1)
 
+#Checks if the system is under pressure
+cpu_usage = 70
+memory_usage = 50
+print(cpu_usage < 90 or memory_usage > 90)
+print(cpu_usage > 70 or memory_usage > 90)
+
+#Checking user creditianls before kofim
+email = True
+password = False
+print(email and password)
