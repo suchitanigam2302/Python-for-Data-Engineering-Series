4b9b08b025f401e28ce3829b5014cfec0a95be6d
suchitanigam <suchitanigam2302@gmail.com>
2026-01-24 06:35:02 +0530

    LogicalOperators

---
+++ 12.LogicalOperators.py
@@ -0,0 +1,10 @@
+#Logical Operators
+
+#and operator---------both condition should be true
+print(3 < 1 and 5 < 1)
+print(3 > 1 and 5 > 1)
+
+#or operator---------- at least one value should be true
+print(3 > 1 or 5 < 1)
+print(3 < 1 or 5 < 1)
+
