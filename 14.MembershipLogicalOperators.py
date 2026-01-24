48043ab5e44e43223ec8778370d21bb9c36c089a
suchitanigam <suchitanigam2302@gmail.com>
2026-01-24 07:43:53 +0530

    IdentityOperators

--- 14.MembershipLogicalOperators.py
+++ 14.MembershipLogicalOperators.py
@@ -12,3 +12,21 @@ print (3 in [1,2,3])
 domain = input("Emter the domain:" )
 is_banned = ["spam.com","fake.org","bot.net"]
 print(domain not in is_banned)
+
+#Identity Operator
+
+x = ['a','b','c']
+y = ['a','b','c']   # y= x points to the same location = True 
+
+print(x==y)
+print(x is y)
+
+a = 10
+b =10
+print(a == b)
+print(a is b)
+
+#Make Sure the email exists,and it not empty
+
+email  = input("Enter your email:" )
+print(email is not None and email != "")
