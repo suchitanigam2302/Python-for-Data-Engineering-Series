1ec1b40fbea5afba65ecbb77a457422f2091f030
suchitanigam <suchitanigam2302@gmail.com>
2026-01-26 22:23:49 +0530

    Multi elif

--- 15.ConditionalStatement.py
+++ 15.ConditionalStatement.py
@@ -3,8 +3,17 @@ score = int(input("Enter the score:" ))
 if score >= 90:
         print("A")
 #elif
-elif score >= 70:
+elif score >= 80:
         print("B")
+#Multi elif
+elif score >= 70:
+        print("C")
+
+elif score >= 60:
+        print("D")
+
+elif score >= 50:
+        print("E")
 # else
 else:
         print("F")
