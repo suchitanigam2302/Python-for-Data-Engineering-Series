4031075561e75b9f503c29ccbdc0dcd701ecdd21
suchitanigam <suchitanigam2302@gmail.com>
2026-02-01 13:05:06 +0530

    Use case 1

--- 18.Loops.py
+++ 18.Loops.py
@@ -6,3 +6,11 @@ for item in range (1,5) :
 string = "Python"
 for str in string :
     print(f"Sequences : {str}")
+
+#Use Case 1
+scores = [60,40,20,30]
+total = 0
+for score in scores :
+    total += score
+    print("Current total :",total)
+print("Final Total :",total)
